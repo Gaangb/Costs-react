9c05a0e1627a04e0cc9203da093c7525e56f6f51
Gaangb <angelob4stos@gmail.com>
2023-09-07 18:47:41 -0300

    feat: adicionando navbar, footer e estilos

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
 import Container from './components/layout/Container';
+import NavBar from './components/layout/NavBar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/company">Company</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/newproject">New Project</Link>
-      </div>
+      <NavBar />
       <Container customClass="min_height">
         <Routes>
           <Route path='/' element={<Home />}/>
@@ -22,7 +19,7 @@ function App() {
           <Route path='/newproject' element={<NewProject />}/>
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer/>
     </Router>
   );
 }
